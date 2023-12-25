91f97c6845dd5c86eb4ce85cf7c4d9a5d14129d6
ckmini66 <kosarmohammed191@gmail.com>
2023-12-25 21:13:22 +0300

    amayan fetch system 4

--- master.js
+++ master.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const TelegramBot = require('node-telegram-bot-api');
 
-// Replace with your Telegram bot token and chat ID
+
 const botToken = '6838747072:AAFroJt42iJkHRJ25n2j-MRNELWSDVf4bMM';
 const chatId = '5167117549';
 
@@ -14,11 +14,11 @@ const links = [
 ];
 
 const checkLinks = async () => {
+  let successCount = 0;
+
   const responses = await Promise.allSettled(links.map(link => axios.get(link.url, { timeout: 20000 })));
 
   const currentTimeStamp = new Date().toLocaleString('en-US', { timeZone: 'Asia/Baghdad' });
-
-  let successCount = 0;
 
   responses.forEach((response, index) => {
     const link = links[index];
@@ -35,12 +35,11 @@ const checkLinks = async () => {
   });
 
   const uptimePercentage = (successCount / links.length) * 100;
-  const reportMessage = `🔍 [Server Monitoring Report 💼]\n\n{Hello Iam Active 😃}\n……………………………………\n 📅 Timestamp: ${currentTimeStamp} 🖥️\n Monitor-Name: Kosar Tarkhany 👨\n\nFetchSystem Overview\n-Status: Online\n\nSpecifications\n- Servers Monitored: ${links.length}\n- Active Pings: 37.36/hour\n- Successful Pings: ${uptimePercentage.toFixed(2)}%\n- Average Response Time: 12 ms\n- Failed Pings: ${(100 - uptimePercentage).toFixed(2)}%\n- Monitoring Uptime: ${uptimePercentage.toFixed(2)}%\n- System Status: Operational\n\n💼 🔍 Server Overview:\n${links.map((link, index) => ` - 🌐 Server ${index + 1}: Online 😊`).join('\n')}\n\n🔧 Status Specifications:\n Messages Processed: 35/hour\n- API Requests: 750/hour\n- GPA Unit: 86 DTR\n- Response Time: 119\n\n🔒 Security Status:\nSystem Firewall: Enabled\n- Intrusion Detection System: Active\n- Security Patches: Up-to-date\n- Encryption: AES-256\n\n📈 Performance Metrics:\n- CPU Utilization: 25%\n- Memory Usage: 60%\n- Response Time: 85 ms\n- Throughput: 5,000 requests/minute\n- Error Rate: 1.5%\n- Network Latency: 20 ms\n\n📊 Resource Utilization:\n- System Health: Normal\n- Services Availability: 99.9%\n- Disk Space: 75% utilized\n- Backup Status: Completed\n- Software Version: 0.64.8\n- Pending Updates: None\n- Network Latency: 15 ms`;
+  const reportMessage = `...`; // (Your report message)
 
   bot.sendMessage(chatId, reportMessage);
 };
 
-// Set up Express server
 const app = express();
 const port = 3000;
 
@@ -49,13 +48,13 @@ app.get('/send-monitoring-report', (req, res) => {
   res.send('Server monitoring report sent!');
 });
 
-// Start Express server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// Set up periodic checking
-setInterval(checkLinks, 60 * 1000);
+setInterval(() => {
+  console.log('Executing scheduled check at:', new Date().toLocaleString());
+  checkLinks();
+}, 12 * 60 * 60 * 1000); 
 
-// Initial check
 checkLinks();
