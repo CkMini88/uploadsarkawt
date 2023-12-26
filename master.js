128cf101852ce4bc2e0fd571831ad329c520b4de
ckmini66 <kosarmohammed191@gmail.com>
2023-12-27 00:35:05 +0300

    amayn fetchsystem7

--- master.js
+++ master.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 const TelegramBot = require('node-telegram-bot-api');
+const dotenv = require('dotenv');
 
+dotenv.config();
 
-const botToken = '6838747072:AAFroJt42iJkHRJ25n2j-MRNELWSDVf4bMM';
+const botToken = process.env.BOT_TOKEN || '6838747072:AAFroJt42iJkHRJ25n2j-MRNELWSDVf4bMM';
 const chatId = '5167117549';
 
 const bot = new TelegramBot(botToken, { polling: false });
@@ -41,10 +43,36 @@ const sendGetRequest = async () => {
 
     await new Promise(resolve => setTimeout(resolve, 20000));
   }
-
-  
 };
 
+// Function to send self-ping
+const sendSelfPing = async () => {
+  const currentTimeStamp = new Date().toLocaleString('en-US', { timeZone: 'Asia/Baghdad' });
+
+  try {
+    const startTime = new Date();
+    console.log(`Sending self-ping at ${startTime.toLocaleString()}`);
+
+    // Use the self-ping URL from the environment variable
+    await axios.get(process.env.SELF_PING_URL, { timeout: 20000 });
+
+    const endTime = new Date();
+    const responseTime = endTime - startTime;
+
+    console.log(`Got a response for self-ping at ${endTime.toLocaleString()} in ${responseTime} milliseconds`);
+
+    if (responseTime > 20000) {
+      const errorMessage = `⚠️ [Alert System Unresponse] ⚠️\n\nAttention: 🚨 The monitoring system has detected a slow response from self-ping.!!\n\nSystem Details 🔍\n………………………………………………….\n👨 Moniter-Name: Kosar Tarkhany 👀\n\n💻 System: Self-Ping\n\n🕒 Time-Stamp: ${currentTimeStamp}\n\n⏱️ Response Time: ${responseTime} milliseconds`;
+
+      bot.sendMessage(chatId, errorMessage);
+    }
+  } catch (error) {
+    console.log(`Error sending self-ping: ${error.message}`);
+    const errorMessage = `⚠️ [Alert System Unresponsive] ⚠️\n\nAttention: 🚨 The monitoring system has detected Self-Ping Unresponsive.!!\n\nSystem Details 🔍\n………………………………………………….\n👨 Moniter-Name: Kosar Tarkhany 👀\n\n💻 System: Self-Ping\n\n🕒 Time-Stamp: ${currentTimeStamp}\n\n🚨 Server-Status: Down\n\n❌ Unresponded-PingNum: 1 Ping`;
+
+    bot.sendMessage(chatId, errorMessage);
+  }
+};
 
 const app = express();
 const port = 3000;
@@ -54,14 +82,24 @@ app.get('/send-monitoring-report', async (req, res) => {
   res.send('Server monitoring report sent!');
 });
 
+// Route for self-ping
+app.get('/self-ping', async (req, res) => {
+  await sendSelfPing();
+  res.send('Self-ping executed!');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-
+// Scheduled task for regular monitoring every 2 minutes
 setInterval(async () => {
   console.log('Executing scheduled check at:', new Date().toLocaleString());
   await sendGetRequest();
-}, 2 * 60 * 1000); 
+}, 2 * 60 * 1000);
 
-sendGetRequest();
+// Scheduled task for self-ping every 1 minute
+setInterval(async () => {
+  console.log('Executing self-ping check at:', new Date().toLocaleString());
+  await sendSelfPing();
+}, 1 * 60 * 1000);
